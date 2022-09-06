6f22ff8029b48fe999ebaf37b375c7ce1a9ff561
Pre-11 <prernakumari7427@gmail.com>
2022-09-06 14:09:05 +0530

    push product files

---
+++ Product page/women.js
@@ -0,0 +1,357 @@
+ var WomenProduct=[
+    {
+        id:1,
+    image_url:
+    "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411597041_RLLZ_1_1800x1800.jpg?v=1661961131",
+
+    image2: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411597041_RLLZ_2_360x.jpg?v=1661961133",
+     brandname: "EMPORIO ARMANI",
+     name: "EMPORIO ARMANI LACE MIDI DRESS",
+     price: 331,
+     strikedoffprice: 1495,
+     save: "78" ,
+
+    },
+
+    {
+        id:2,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411962836_RLLZ_1_1800x1800.jpg?v=1662109478",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411962836_RLLZ_2_large.jpg?v=1662109479",
+        brandname:"EMPORIO ARMANI",
+        name:"EMPORIO ARMANI SHIFT DRESS",
+        price: 199 ,
+        strikedoffprice: 1295,
+        save: 85,
+
+    },
+    {
+        id:3,
+    image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411212007_RLLZ_1_1800x1800.jpg?v=1662171766",
+
+    image2:
+    "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411212007_RLLZ_2_large.jpg?v=1662171768",
+
+     brandname:"EMPORIO ARMANI",
+     name: "WOMENS RUFFLED FULL LENGTH EVENING DRESS",
+     price: 164,
+     strikedoffprice: 1595,
+    save:"Save 90",
+
+    },
+
+    {
+        id:4,
+        image_url: 
+        "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/WH7_GLX-873499_Nero_1800x1800.jpg?v=1652415240",
+
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/WH7_GLX-873499_Nero_557f8106-8dbb-4d5c-bc7b-3de48af709f5_large.jpg?v=1654581275",
+        brandname:"GIORGIO ARMANI",
+        name: "EMPORIO ARMANI WOMEN'S DRESS IN BLACK ",
+        price: 589,
+        strikedoffprice: 778,
+        save:"Save 24",
+
+    },
+
+    {
+        id:5,
+        image_url:
+       "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/xPGzJ9v1zb9XhIKiLVHKBakUftQLRmNd-25_ee86b5d2-d5a4-4c35-8e75-1947f9b76a43_1800x1800.jpg?v=1661694914",
+
+       image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/gxAJWFt5IkUbJBC5oG6z1qWpq4OG8D7E-25_large.jpg?v=1661694915",
+
+        brandname: "BCBGMAXAZRIA",
+        name: "VIOLET WOMENS METALLIC PLEATED EVENING DRESS",
+        price: "173",
+        strikedoffprice: "448",
+        save: "Save 61",
+
+    },
+
+    {
+        id:6,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452358018_RLLZ_1_360x.jpg?v=1662341157",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452358018_RLLZ_2_360x.jpg?v=1662341160",
+        brandname: "SACHIN  BABI",
+        name: "SACHIN + BABI HANNAH GOWN",
+        price:"298.00",
+        strikedoffprice:"895",
+        save:"Save 67",
+
+    },
+
+    {
+        id:7,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/5m1re7r6ywA0Sk4vlPuifdRDwFmDIA60-25_540x.jpg?v=1662028537",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/u41N8g1fzPGKwqzKrNqk5jEzWjkpgWKI-25_large.jpg?v=1662028538",
+        brandname: "BCBGMAXAZRIA",
+        name: "WOMENS RUFFLED CORSET EVENING DRESS",
+        price:"498",
+        strikedoffprice: "600",
+        save:"Save 37",
+
+    },
+    {
+        id:8,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411554818_RLLZ_1_093feddc-7744-49b9-ad2e-bb454669352c_360x.jpg?v=1661987407",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411554818_RLLZ_2_360x.jpg?v=1661987409",
+        brandname: "JOHNNY WAS",
+        name: "JOHNNY WAS DORIS SILK",
+        price:"199",
+        strikedoffprice:"365.00",
+        save:"Save 45",
+
+    },
+
+    {
+        id:9,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411195597_RLLZ_1_88efb6a8-7dc8-4e02-86e5-d4ed7f00175b_360x.jpg?v=1662165768",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411195597_RLLZ_2_large.jpg?v=1662165770",
+        brandname:"VINCE",
+        name:"VINCE PANELED SLIP DRESS",
+        price:"179",
+        strikedoffprice:"445",
+        save:"Save 60",
+
+    },
+
+    {
+        id:10,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452173629_RLLZ_1_360x.jpg?v=1661744168",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452173629_RLLZ_2_360x.jpg?v=1661744169",
+        brandname:"HERVE LEGER",
+        name: "HERVE LEGER FRINGE SHEATH DRESS",
+        price:"899",
+        strikedoffprice:"1,990",
+        save:"Save 55",
+
+    },
+
+    {
+        id:11,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411385099_RLLZ_1_360x.jpg?v=1662131262",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411385099_RLLZ_2_360x.jpg?v=1662131264",
+        brandname:"A.L.C.",
+        name:"A.L.C. GWEN MIDI DRESS",
+        price:"199",
+        strikedoffprice:"695",
+        save:"Save 71",
+
+    },
+
+    {
+        id:12,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411195597_RLLZ_1_88efb6a8-7dc8-4e02-86e5-d4ed7f00175b_360x.jpg?v=1662165768",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1411195597_RLLZ_2_360x.jpg?v=1662165770",
+        brandname:"VINCE",
+        name:"VINCE PANELED SLIP DRESS",
+        price:"179",
+        strikedoffprice:"445",
+        save:"Save 60",
+    },
+
+
+    {
+        id:13,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452350202_RLLZ_1_360x.jpg?v=1662163599",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1452350202_RLLZ_2_360x.jpg?v=1662163608",
+        brandname:"AIDAN MATTOX",
+        name: "AIDAN MATTOX BEADED GOWN",
+        price:"166",
+        strikedoffprice:"550",
+        save:"Save 70",
+
+    },
+
+
+    {
+        id:14,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/7s53UDmAQlkVJLDe5tw425pZBr3gCwgJ-25_360x.jpg?v=1661833851",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/cUnVp3LxOn9V8j4NESwUuKmyWJZeR8il-25_360x.jpg?v=1661833852",
+        brandname: "AIDAN BY AIDAN MATTOX",
+        name:"WOMENS SUMMER KIMONO SLEEVE MINI DRESS",
+        price:"33",
+        strikedoffprice:"175",
+        save:"Save 81 ",
+
+    },
+
+    {
+        id:15,
+        image_url: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/KMwifaILHRpS0XNagh7eDMswjOWl47RD-25_360x.jpg?v=1662360674",
+        image2:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/OcLDmVZlWnnGnvG0Ea3vfNopMLoVMXqs-25_360x.jpg?v=1662360676",
+        brandname:"AIDAN MATTOX",
+        name:"AIDAN MATTOX WOMENS OFF-THE-SHOULDER",
+        price:"169",
+        strikedoffprice:"395",
+        save:"Save 57",
+
+    }
+    
+];
+
+
+  WomenProduct.map(function(elem) { 
+
+    var divs=document.createElement("div");
+    divs.setAttribute("class", "product_box")
+
+    var image=document.createElement("img");
+     image.setAttribute("id", "Image_id") 
+     image.setAttribute("src",elem.image_url);
+
+    var image_box = document.createElement("div");
+    image_box.setAttribute("class", "image_box")
+
+   
+   image_box.addEventListener("mouseout",  function() {
+    image.setAttribute("src",elem.image_url);
+   
+})
+   
+        image_box.addEventListener("mouseover", function () {
+           image.setAttribute("src",elem.image2);
+            
+        });
+       
+    
+     
+    image_box.append(image);
+
+
+    var h5=document.createElement("h5");
+    h5.innerText=elem.brandname;
+    h5.setAttribute("Id","brand_Name");
+
+    var h4=document.createElement("h4");
+    h4.innerText=elem.name;
+    h4.setAttribute("Id","NAME")
+
+    var priceBox=document.createElement("div");
+
+    var price=document.createElement("p");
+    price.innerText="$"+elem.price;
+
+    price.setAttribute("class", "price_line")
+     
+    var sp=document.createElement("p");
+    sp.innerText="$"+elem.strikedoffprice +".99";
+
+    var save=document.createElement("p");
+    save.innerText=elem.save+"%";
+     
+     save.setAttribute("class", "save_line")
+
+    priceBox.append(price,sp,save);
+    divs.append(image_box,h5,h4,priceBox);
+    var Product =  document.getElementById("Product")
+    Product.append(divs);
+    document.querySelector("#Parent").append(Product);
+
+
+    divs.addEventListener("click",function(){
+
+        myPage(elem)
+
+    
+
+
+            
+
+        
+
+        
+    });
+});
+
+
+
+
+// WomenProduct.map(function(elem) { 
+
+
+    var Arr=[];
+function myPage(elem){
+    
+    // console.log(elem.price);
+    
+    var Obj={
+        image:elem.image_url,
+        secImage:elem.image2,
+        brandName:elem.brandname,
+        name:elem.name,
+        price:elem.price,
+        sp:elem.strikedoffprice,
+        save:elem.save,
+    }
+    Arr.push(Obj);
+    localStorage.setItem("ProductDetails" ,JSON.stringify(Arr));
+    Arr=[];
+}
+
+
+
+
+
+
+
+// var GetData=document.getElementsByClassName("product_box")
+
+
+//  Array.from(GetData).forEach(element => {
+//     element.addEventListener("click", myPage )
+    
+//   function myPage() {
+
+//     event.preventDefault();
+
+//     var Arr=[];
+
+//     var obj = { 
+
+//        
+
+
+//         // "pro_img":
+//         // "brand_name" : 
+//         // "name" : 
+//         // "price ": elem.price,
+//         // "sp_price": elem.strikedoffprice
+
+    
+
+//    }
+
+        
+//         Arr.push(obj);
+          
+//         localStorage.setItem("data" , JSON.stringify(Arr) );
+
+
+// }
+// });
+// });
+
+
+  
+// var opt = document.getElementsByClassName("options")
+// var collection = document.getElementsByClassName("collections")
+
+// function hide(collection){
+//     for(var i=0; i<opt.length; i++){
+//      if(opt[i].style.display=="none"){
+//         opt[i].style.display = "block";
+//     }
+//     else{
+//         opt[i].style.display = "none"
+      
+//     }
+
+//     }
+  
+//  }
+
+
+
+
+
