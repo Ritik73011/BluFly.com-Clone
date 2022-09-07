b840a3d0c91aa0ff39749e852e181df48c6392d6
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-09-07 17:03:06 +0530

    storing data to local storage

--- Product_Details_Page/details.js
+++ Product_Details_Page/details.js
@@ -11,7 +11,8 @@ for(var i =0; i<1;i++)
  document.getElementById("image").setAttribute("src",cart[i].img);
 }*/
 
-//For Big Screen
+
+//For Big Screen Start
  var img=document.getElementById("img1").src;
  var img2=document.getElementById("img2").src;
  var img3=document.getElementById("img3").src;
@@ -27,8 +28,9 @@ document.getElementById("img2").addEventListener("click",function(){
    document.getElementById("img3").addEventListener("click",function(){
     document.getElementById("image").setAttribute("src",img3);
    });
+//Big Image Screen End
 
-   //For Small Screen
+   //For Small Screen Start
    var mimg=document.getElementById("mimg1").src;
    var mimg2=document.getElementById("mimg2").src;
    var mimg3=document.getElementById("mimg3").src;
@@ -44,3 +46,73 @@ document.getElementById("img2").addEventListener("click",function(){
      document.getElementById("mimg3").addEventListener("click",function(){
       document.getElementById("image").setAttribute("src",mimg3);
      });
+ // Small Image End
+
+
+ //BUTTON CLICK START
+ document.getElementById("addCart").addEventListener("click",function(){
+    setProductDetailsToLocalStorage();
+ });
+
+//BUTTON CLICK END
+
+
+  //Radio BUTTON Start
+  var radio="";
+ function radioChecked(){
+   if(document.getElementById('s').checked)
+   {
+    radio="S";
+   }
+   else if(document.getElementById('m').checked)
+   {
+    radio="M";
+   }
+   else if(document.getElementById('l').checked)
+   {
+    radio="L";
+   }
+   else if(document.getElementById('xl').checked)
+   {
+    radio="XL";
+   }
+   else{
+
+   }
+ }
+
+ //radio Button End
+
+
+ //LOCAL STROAGE START
+
+ var data = JSON.parse(localStorage.getItem("cartData")) || [];
+ function setProductDetailsToLocalStorage()
+ {
+    radioChecked();
+    if(radio===""){
+        alert("please select size");
+    }
+    else{
+        var parseImg = document.getElementById("image").src;
+        var parseProductName = document.getElementById("productName").innerText;
+        var parseSize = radio;
+        var parsePrice = document.getElementById("ogPrice").innerText;
+
+        //console.log(parseImg,parseProductName,parseSize,parsePrice);
+
+        var obj = {
+            pName:parseProductName,
+            pPrice:parsePrice,
+            pSize:parseSize,
+            pImg:parseImg
+        };
+
+        data.push(obj);
+
+        localStorage.setItem("cartData",JSON.stringify(data));
+    }
+ }
+
+
+ //LOCAL STROAGE END
